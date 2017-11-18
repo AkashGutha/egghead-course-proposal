52c08308a07bf318c2d1d3babd6a49bfbad2f97b
AkashGutha <akash.gutha@outlook.com>
2017-11-18 12:46:07 +0530

    added dynamic lessons

--- src/screens/CourseProposal/index.js
+++ src/screens/CourseProposal/index.js
@@ -1,21 +1,26 @@
-import React, {Component} from 'react'
-import Header from './components/Header'
-import CourseInfoInput from './components/CourseInfoInput'
-import CourseInfoRow from './components/CourseInfoRow'
-import CourseLessonList from './components/CourseLessonList'
-import {css} from 'glamor'
+import React, { Component } from "react";
+import Header from "./components/Header";
+import CourseInfoInput from "./components/CourseInfoInput";
+import CourseInfoRow from "./components/CourseInfoRow";
+import CourseLessonList from "./components/CourseLessonList";
+import { css } from "glamor";
+import { inject, observer } from "mobx-react";
 
+@inject()
+@observer
 class CourseProposal extends Component {
   render() {
+    const lessons = ['1', '2', '3'];
+
     return (
-      <div className={`w-100 h-100 bg-gray ${css({margin: '0 auto'})}`}>
+      <div className={`w-100 h-100 bg-gray ${css({ margin: "0 auto" })}`}>
         <Header />
         <CourseInfoInput />
         <CourseInfoRow />
-        <CourseLessonList lessonCount="0"/>
+        <CourseLessonList lessons={lessons} />
       </div>
-    )
+    );
   }
 }
 
-export default CourseProposal
+export default CourseProposal;
