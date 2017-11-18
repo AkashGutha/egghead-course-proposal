c5a42ef32151f5347d04682cffdfd30d5c7f72f0
AkashGutha <akash.gutha@outlook.com>
2017-11-18 22:34:12 +0530

    changed route to course edit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import {StaticRouter} from 'react-router'
 import CourseProposal from './screens/CourseProposal'
+import CourseEdit from "./screens/CourseEdit";
 
 class App extends Component {
   render() {
     return (
       <StaticRouter context={{}}>
-          <div className='bg-base vh-100'>
-              <CourseProposal />
-              
+          <div className='bg-white-secondary vh-100'>
+              <CourseEdit />
           </div>
       </StaticRouter>
     )
